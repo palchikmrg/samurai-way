124395c57ff2630feb07fb912dc629fa5908b6ea
palchikmrg <palchik.mrg@gmail.com>
2022-06-28 22:43:08 +0300

    add grids

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 
 
-
 function App() {
     return (
-        <div>
-            <Header />
-            <Technologies />
-            <Footer />
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn'></img>
+                </header>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div></div>
+            </nav>
+            <div className='content'>Main content</div>
 
         </div>
-    );
+);
 }
 
 
